be1428fb3609d43ec005623ddc4cf61e3e17f5de
Jonathan Lalande <connect@jonathanlalande.com>
2025-07-03 16:46:58 -0700

    Configure headers for Storyblok preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,18 @@ import { defineConfig } from "astro/config";
 import { storyblok } from "@storyblok/astro";
 
 export default defineConfig({
+  response: {
+    headers: {
+      // This allows Storyblok's visual editor to embed your site
+      "Content-Security-Policy": "frame-ancestors https://app.storyblok.com",
+    },
+  },
   integrations: [
     storyblok({
       accessToken: "Q3jH6AtV98UxC2agQQklaAtt",
       components: {
         page: "storyblok/Page",
-        hero_section: "storyblok/HeroSection", // Add this line
+        hero_section: "storyblok/HeroSection",
       },
       apiOptions: {
         region: "ca",
